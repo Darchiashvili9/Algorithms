15daee1ba2502396e329f83e77276fd0989e5130
Darchiashvili9 <gdarchiashvili@hotmail.com>
2026-08-25 21:11:28 +0400

    Add Two Sum solution

---
+++ Algorithms/ArraysAndHashing/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithms.ArraysAndHashing
+{
+    public class TwoSum
+    {
+        public static int[] Solve(int[] nums, int target)
+        {
+            Dictionary<int, int> seen = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = target - nums[i];
+
+                if (!seen.ContainsKey(num))
+                {
+                    if (seen.ContainsKey(nums[i]))
+                        continue;
+
+                    seen.Add(nums[i], i);
+                }
+                else
+                {
+                    return new int[] { seen[num], i };
+                }
+            }
+
+            throw new InvalidOperationException("No solution found.");
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Algorithms
+﻿using Algorithms.ArraysAndHashing;
+
+namespace Algorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string s = "aab";
+            int[] nums = { 1, 1, 2, 3 };
+            int [] indexies = TwoSum.Solve(nums, 6);
+
+            foreach (var item in indexies)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
