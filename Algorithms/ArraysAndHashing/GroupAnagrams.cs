e4a0f483b631391ccd8ca435526f8bc8696a2543
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-08-28 11:45:26 +0400

    Add Group Anagrams Solution

---
+++ Algorithms/ArraysAndHashing/GroupAnagrams.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithms.ArraysAndHashing
+{
+    public class GroupAnagrams
+    {
+        public static IList<IList<string>> Solve(string[] strs)
+        {
+            Dictionary<string, List<string>> groups = new();
+
+            foreach (var word in strs)
+            {
+                char[] chars = word.ToCharArray();
+                Array.Sort(chars);
+                string key = new string(chars);
+
+
+                if (!groups.ContainsKey(key))
+                {
+                    groups.Add(key, new List<string> { word });
+                }
+                else
+                {
+                    groups[key].Add(word);
+                }
+            }
+
+            return groups.Values.Select(val => (IList<string>)val).ToList();
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,6 +6,8 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            #region TwoSum
+            /*
             int[] nums = { 1, 1, 2, 3 };
             int [] indexies = TwoSum.Solve(nums, 6);
 
@@ -13,6 +15,19 @@ namespace Algorithms
             {
                 Console.WriteLine(item);
             }
+            */
+            #endregion
+
+            #region GroupAnagrams
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+            var result = GroupAnagrams.Solve(strs);
+
+            foreach (var group in result)
+            {
+                Console.WriteLine(string.Join(", ", group));
+            }
+            #endregion
         }
     }
 }
