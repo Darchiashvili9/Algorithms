5c043361913b70516778e3bd748ee8bed54fcef1
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-09-18 22:36:42 +0400

    updated Binary Search solution

--- Algorithms/BinarySearch/BinarySearch.cs
+++ Algorithms/BinarySearch/BinarySearch.cs
@@ -7,25 +7,23 @@
             int left = 0;
             int right = nums.Length - 1;
 
-            while (left < right)
+            while (left <= right)
             {
                 int mid = left + (right - left) / 2;
 
                 if (nums[mid] == target)
-                {
+
                     return mid;
-                }
+
                 if (target < nums[mid])
-                {
+
                     right = mid - 1;
-                    mid = left + (right - left) / 2;
-                }
+
 
                 if (target > nums[mid])
-                {
+
                     left = mid + 1;
-                    mid = left + (right - left) / 2;
-                }
+
             }
 
             return -1;
