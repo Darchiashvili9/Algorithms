10d1e00b760f4eb640b440119dccf412544066c3
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-09-18 22:38:22 +0400

    Clean up Binary Search formatting

--- Algorithms/BinarySearch/BinarySearch.cs
+++ Algorithms/BinarySearch/BinarySearch.cs
@@ -12,20 +12,14 @@
                 int mid = left + (right - left) / 2;
 
                 if (nums[mid] == target)
-
                     return mid;
 
                 if (target < nums[mid])
-
                     right = mid - 1;
 
-
                 if (target > nums[mid])
-
                     left = mid + 1;
-
             }
-
             return -1;
         }
     }
